4daa65dbbe395628388cdbd552e03e9d36e09974
Matthew Nestor <mtthwnestor@gmail.com>
2017-10-03 12:56:20 -0400

    Load and render a model.

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ class MyApp(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
 
+        # Load a model and render it.
+        self.scene = self.loader.loadModel('models/environment')
+        self.scene.reparentTo(self.render)
+
+        # Set the model's position and scale.
+        self.scene.setScale(0.25, 0.25, 0.25)
+        self.scene.setPos(-8, 42, 0)
+
 
 if __name__ == '__main__':
 
